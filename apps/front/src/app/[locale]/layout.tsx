44407d2378676156f8fc0994eff405253bf21bfd
Miłosz Wiśniewski <milosz@uigstudio.com>
2025-01-18 08:53:07 +0100

    setup 3 - base paths + types

--- apps/front/src/app/[locale]/layout.tsx
+++ apps/front/src/app/[locale]/layout.tsx
@@ -3,7 +3,7 @@ import {NextIntlClientProvider} from 'next-intl';
 import {notFound} from 'next/navigation';
 import {type ReactNode} from 'react';
 import {routing} from '@front/i18n/routing';
-import HtmlHead from '@lib/front/components/atoms/HtmlHead';
+import type {Metadata} from 'next';
 
 import '../style.css';
 
@@ -14,6 +14,42 @@ export type LocaleParams = {
 export type LocaleLayoutProps = {
     children: ReactNode;
     params: Promise<LocaleParams>;
+};
+
+const Head = ({title, description}: Pick<Metadata, 'title' | 'description'>) => {
+    return (
+        <head>
+            <link
+                rel="apple-touch-icon"
+                sizes="180x180"
+                href="/favicon/apple-touch-icon.png"
+            />
+            <link
+                rel="icon"
+                type="image/png"
+                sizes="32x32"
+                href="/favicon/favicon-32x32.png"
+            />
+            <link
+                rel="icon"
+                type="image/png"
+                sizes="16x16"
+                href="/favicon/favicon-16x16.png"
+            />
+            <link rel="manifest" href="/favicon/site.webmanifest" />
+            <link
+                rel="mask-icon"
+                href="/favicon/safari-pinned-tab.svg"
+                color="#000000"
+            />
+            <link rel="shortcut icon" href="/favicon/favicon.ico" />
+            <meta name="msapplication-TileColor" content="#000000" />
+            <meta name="msapplication-config" content="/favicon/browserconfig.xml" />
+            <meta name="theme-color" content="#000" />
+            <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
+            <title>{title}</title>
+        </head>
+    );
 };
 
 async function LocaleLayout({children, params}: LocaleLayoutProps) {
@@ -27,7 +63,7 @@ async function LocaleLayout({children, params}: LocaleLayoutProps) {
 
     return (
         <>
-            <HtmlHead seo={pageSeo} />
+            <Head />
             <NextIntlClientProvider messages={messages}>
                 {children}
             </NextIntlClientProvider>
