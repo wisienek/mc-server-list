7f2ffecf8ccefba4ec5b76fb0cb604a673fcd3da
Miłosz Wiśniewski <milosz@uigstudio.com>
2025-02-01 20:58:35 +0100

    testing env setup

--- apps/server/jest.config.ts
+++ apps/server/jest.config.ts
@@ -1,10 +1,11 @@
 export default {
-  displayName: 'server',
-  preset: '../../jest.preset.js',
-  testEnvironment: 'node',
-  transform: {
-    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }]
-  },
-  moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../coverage/apps/server'
+    displayName: 'server',
+    preset: '../../jest.preset.js',
+    setupFiles: ['../../jest-setup.ts'],
+    testEnvironment: 'node',
+    transform: {
+        '^.+\\.[tj]s$': ['ts-jest', {tsconfig: '<rootDir>/tsconfig.spec.json'}],
+    },
+    moduleFileExtensions: ['ts', 'js', 'html'],
+    coverageDirectory: '../../coverage/apps/server',
 };
