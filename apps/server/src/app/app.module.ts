94b24c5cea212e63f537a4a7aebc66862b4b5dab
Miłosz Wiśniewski <milosz@uigstudio.com>
2025-02-01 21:20:57 +0100

    modules

--- apps/server/src/app/app.module.ts
+++ apps/server/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import {ModuleMetadata} from '@nestjs/common/interfaces/modules/module-metadata.interface';
 import {EventEmitterModule} from '@nestjs/event-emitter';
 import {Module, type Provider} from '@nestjs/common';
 import {AutomapperModule} from '@automapper/nestjs';
@@ -6,10 +7,13 @@ import {CqrsModule} from '@nestjs/cqrs';
 import {ApiConfig, getConfigs, ProjectConfig} from '@backend/config';
 import {LoggerModule} from '@backend/logger';
 import {DataBaseModule} from '@backend/db';
+import {ServersModule} from '../servers';
 
 const interceptors: Provider[] = [];
 
 const configs = getConfigs(ProjectConfig, ApiConfig);
+
+const serverModules: ModuleMetadata['imports'] = [DataBaseModule, ServersModule];
 
 @Module({
     imports: [
@@ -20,7 +24,7 @@ const configs = getConfigs(ProjectConfig, ApiConfig);
             strategyInitializer: classes(),
         }),
         ...configs,
-        DataBaseModule,
+        ...serverModules,
     ],
     providers: [...interceptors],
 })
