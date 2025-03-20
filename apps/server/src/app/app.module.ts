e62b9debadf640e1ffd52b5a71ee23a1918199ec
Miłosz Wiśniewski <milosz@uigstudio.com>
2025-03-20 23:02:34 +0100

    temporarily removed caching layer

--- apps/server/src/app/app.module.ts
+++ apps/server/src/app/app.module.ts
@@ -1,6 +1,5 @@
 import {RedisModule} from '@backend/redis';
 import {createKeyv, Keyv} from '@keyv/redis';
-import {CacheModule} from '@nestjs/cache-manager';
 import type {ModuleMetadata} from '@nestjs/common/interfaces/modules/module-metadata.interface';
 import {APP_GUARD} from '@nestjs/core';
 import {EventEmitterModule} from '@nestjs/event-emitter';
@@ -58,26 +57,6 @@ const serverModules: ModuleMetadata['imports'] = [
         ...serverModules,
         TypeOrmModule.forFeature([Session]),
         RedisModule,
-        CacheModule.registerAsync({
-            imports: [...getConfigs(RedisConfig)],
-            isGlobal: true,
-            useFactory: async (config: RedisConfig) => {
-                return {
-                    stores: [
-                        new Keyv({
-                            store: new CacheableMemory({
-                                ttl: config.DEFAULT_CACHE_TIME,
-                                lruSize: 5_000,
-                            }),
-                        }),
-                        createKeyv(
-                            `redis://${config.REDIS_HOST}:${config.REDIS_PORT}`,
-                        ),
-                    ],
-                };
-            },
-            inject: [RedisConfig],
-        }),
     ],
     providers: [...interceptors, ...guards],
 })
