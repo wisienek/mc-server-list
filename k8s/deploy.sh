95f92be1e9637506e645e579e3c5d6fe17c0be06
Miłosz Wiśniewski <milosz@uigstudio.com>
2025-03-24 02:03:34 +0100

    configs + correct front dockerfile

--- k8s/deploy.sh
+++ k8s/deploy.sh
@@ -11,7 +11,8 @@ kubectl create secret generic mc-secrets \
   --from-literal=COOKIE_SECRET="${COOKIE_SECRET}" \
   --from-literal=DISCORD_CLIENT_ID="${DISCORD_CLIENT_ID}" \
   --from-literal=DISCORD_CLIENT_SECRET="${DISCORD_CLIENT_SECRET}" \
-  --from-literal=DISCORD_REDIRECT_URI="${DISCORD_REDIRECT_URI}"
+  --from-literal=DISCORD_REDIRECT_URI="${DISCORD_REDIRECT_URI}" \
+  --from-literal=POSTGRES_PASSWORD="${POSTGRES_PASSWORD}"
 
 echo "Applying Kubernetes Deployments..."
 kubectl apply -f $DEPLOYMENT_DIR/postgres-deployment.yaml
